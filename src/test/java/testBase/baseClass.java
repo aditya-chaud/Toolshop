9420676e3972ad5927894fbe9d11544b8a90b2bc
aditya-chaud <chaudariaditya@gmail.com>
2025-06-08 00:11:28 +0545

    presignup

--- src/test/java/testBase/baseClass.java
+++ src/test/java/testBase/baseClass.java
@@ -1,4 +1,25 @@
 package testBase;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
 public class baseClass {
+    public WebDriver driver;
+    @BeforeClass
+    public void setUp(){
+        driver=new ChromeDriver();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        driver.get("https://www.automationexercise.com/");
+        driver.manage().window().maximize();
+    }
+
+//    @AfterClass
+//    public void tearDown(){
+//        driver.quit();
+//    }
 }
--- src/test/java/testCases/signUpLoginTest.java
+++ src/test/java/testCases/signUpLoginTest.java
@@ -1,6 +1,17 @@
 package testCases;
 
-public class signUpLoginTest {
+import org.testng.annotations.Test;
+import pageObjects.SignupLoginPage;
+import pageObjects.homePage;
+import testBase.baseClass;
 
-
+public class signUpLoginTest extends baseClass {
+    @Test
+    public void presignup(){
+        homePage hp=new homePage(driver);
+        hp.setsignUpLogin();
+        SignupLoginPage signup=new SignupLoginPage(driver);
+        signup.setSignupName("testad05");
+        signup.setSignupEmail("testad05@gmail.com");
+    }
 }
